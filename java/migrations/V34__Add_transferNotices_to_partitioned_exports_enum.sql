8941bf71780f2a0d91c1d81cb42b226ecce44186
Michael ANTOUN <michael.antoun@fabernovel.com>
2022-05-24 17:13:46 +0200

    Add new migrations files

---
+++ java/migrations/V34__Add_transferNotices_to_partitioned_exports_enum.sql
@@ -0,0 +1,25 @@
+
+ALTER TABLE partitioned_exports
+    MODIFY COLUMN
+        resource enum (
+        'sentTransfers',
+        'transferRequests',
+        'users',
+        'stores',
+        'products',
+        'stocklevels',
+        'stockrooms',
+        'zones',
+        'productsDescriptionsTranslations',
+        'productsSize',
+        'sellers',
+        'userGroupAuthorizations',
+        'collections',
+        'materials',
+        'customerOrders',
+        'availableOrders',
+        'customers',
+        'deposits',
+        'languagePreferences',
+        'transferNotices'
+        ) NOT NULL;
---
+++ java/migrations/V37__Add_customerReservations_to_partitioned_exports_enum.sql
@@ -0,0 +1,26 @@
+
+ALTER TABLE partitioned_exports
+    MODIFY COLUMN
+        resource enum (
+        'sentTransfers',
+        'transferRequests',
+        'users',
+        'stores',
+        'products',
+        'stocklevels',
+        'stockrooms',
+        'zones',
+        'productsDescriptionsTranslations',
+        'productsSize',
+        'sellers',
+        'userGroupAuthorizations',
+        'collections',
+        'materials',
+        'customerOrders',
+        'availableOrders',
+        'customers',
+        'deposits',
+        'languagePreferences',
+        'transferNotices',
+        'customerReservations'
+        ) NOT NULL;
